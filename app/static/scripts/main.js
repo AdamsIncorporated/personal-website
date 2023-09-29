13fa1b50ca064f0d7859da181e8b6eb6c62a9c17
Samuel Adams <sadams@brb.state.tx.us>
2023-09-29 14:34:26 -0500

    Added new butttons

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector(".navbar");
-    const sections = document.querySelectorAll(".section");
-
-    // Function to reveal the navbar when scrolling down (unchanged)
-
-    // Initialize ScrollReveal for sections
     ScrollReveal().reveal('.section', {
         delay: 200,
         distance: '50px',
@@ -13,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
         duration: 1000, // Duration of the animation in milliseconds
         easing: 'ease-in-out', // Easing function for the animation
     });
+});
 
-    // Event listener for scrolling (unchanged)
+const pushbar = new Pushbar({
+    blur: true,
+    overlay: true,
 });
