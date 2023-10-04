ca474d87c2ae8b0e848975a5489d9b2708821dbc
Samuel Adams <sadams@brb.state.tx.us>
2023-10-04 13:49:06 -0500

    changed shadow for resume

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -10,11 +10,19 @@ ScrollReveal().reveal(".section", {
     },
 });
 
+ScrollReveal().reveal(".resume", {
+    duration: 500,
+    delay: 200,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in",
+    afterReveal: (el) => {
+        el.style.opacity = 1;
+    },
+});
+
 // Pushbar button
 const pushbar = new Pushbar({
     blur: true,
     overlay: true,
 });
-
-
-$(".nano").nanoScroller();
