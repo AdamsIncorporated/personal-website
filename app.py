f3667423db74f045f4a04d01a0ee85a7cc1f3794
Samuel Adams <samuel.grant.adams@gmail.com>
2023-10-22 16:34:54 -0500

    changed runtime

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = create_app()
 def main():
 
     # The reloader has not yet run - open the browser
-    # if not os.environ.get("WERKZEUG_RUN_MAIN"):
-    #     webbrowser.open_new("http://127.0.0.1:5000/")
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new("http://127.0.0.1:5000/")
 
     # Otherwise, continue as normal
     app.run(host="127.0.0.1", debug=True, port=5000)
