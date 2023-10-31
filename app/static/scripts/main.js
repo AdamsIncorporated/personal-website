7feaaf298aa52704746581e7f196060302bc11cc
Samuel Adams <sadams@brb.state.tx.us>
2023-10-31 13:41:17 -0500

    added jupyter inspired code blocks

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -19,3 +19,29 @@ const pushbar = new Pushbar({
     blur: true,
     overlay: true,
 });
+
+// Function to copy code to the clipboard
+function copyCodeToClipboard() {
+    const container = document.querySelector('.jupyter-code-block');
+    const originalBorderColor = getComputedStyle(container).borderColor;
+
+    // Change the border color to green
+    container.style.borderColor = '#54aa18';
+
+    // Add a CSS transition for a smooth change
+    container.style.transition = 'border-color 1s ease';
+
+    // Set a timeout to return the border color to the original color after 1 second
+    setTimeout(() => {
+        container.style.borderColor = originalBorderColor;
+    }, 1000); // Change the timeout to 1000 milliseconds (1 second)
+
+    const codeBlock = container.querySelector('pre');
+    const textArea = document.createElement('textarea');
+    textArea.value = codeBlock.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+}
+
